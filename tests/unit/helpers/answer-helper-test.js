2bd1e27abb60df01c207f8c2e11f980f49315c06
Peter Beach <peterbeach2@gmail.com>
2016-06-17 09:29:40 -0700

    added custom helped that displays question mark icon next to unanswered questions.

---
+++ tests/unit/helpers/answer-helper-test.js
@@ -0,0 +1,10 @@
+import { answerHelper } from 'qa-board/helpers/answer-helper';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | answer helper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = answerHelper([42]);
+  assert.ok(result);
+});
