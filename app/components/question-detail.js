070a22d60a02c5f597e305ae619a5e5c9ed9e3e2
Peter Beach <peterbeach2@gmail.com>
2016-06-17 11:25:31 -0700

    added ember service that allows the user to add a questions to a list of favorite questions. The list of favorites questions is displayed in the favorites route and contains links to each favorite question.

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  favoriteQuestions: Ember.inject.service(),
+  actions: {
+    addToFavorites(question){
+      this.get("favoriteQuestions").add(question);
+    }
+  },
   answerNumber: Ember.computed('question.answers.length', function() {
     return this.get('question.answers.length');
   })
